3298a055312ce88ace8a665c860f09e36a1b595a
dougshipp <doug.shipp@atomicobject.com>
2022-04-27 01:53:02 -0400

    modules

--- src/test/SpritelyGenesis.t.sol
+++ src/test/SpritelyGenesis.t.sol
@@ -1,21 +1,30 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
-import "ds-test/test.sol";
+import "@std/Test.sol";
+import "./utils/Cheats.sol";
 import "../SpritelyGenesis.sol";
 
-contract SpritelyGenesisTest is DSTest {
+
+contract SpritelyGenesisTest is Test {
+    Cheats internal constant cheats = Cheats(HEVM_ADDRESS);
     uint8 public constant DECIMALS = 18;
     int256 public constant INITIAL_ANSWER = 1 * 10**18;
     SpritelyNFT public spritelyNFT;
+
 
     function setUp() public {
         spritelyNFT = new SpritelyNFT();
     }
 
-    function test_is_sale_active() public {
+    function test_sale_is_active() public {
+        spritelyNFT.setSaleIsActive(true);
         bool saleIsActive = spritelyNFT.saleIsActive();
         assertTrue(saleIsActive);
     }
+
+    // function test_mint(uint num) public {
+    //     spritelyNFT.mint(num);
+    // }
 }
